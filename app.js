6461c423ca93b94fb98d89c9e522f6f226afa474
Gabriel Peña <gabriel@atiko7.com>
2015-09-07 18:15:06 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require("express"),
+    app = express(),
+    bodyParser  = require("body-parser"),
+    methodOverride = require("method-override");
+    mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+   res.send("Hello World!");
+});
+
+app.use(router);
+
+app.listen(3000, function() {
+  console.log("Node server running on http://localhost:3000");
+});
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+    if(err) throw err;
+    console.log('Connected to Database');
+});
+
+var models = require('./models/tvshow')(app, mongoose);
+
+var TVShowCtrl = require('./controllers/tvshows');
+
+console.log(TVShowCtrl);
+
+// API routes
+var tvshows = express.Router();
+
+tvshows.route('/tvshows')
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
